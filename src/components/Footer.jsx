c4dabe2ffcc41c40f3ccfda0398a70d6e1c344e8
Jonathan Kosasih <jonathankosasih@gmail.com>
2024-05-05 22:17:48 +0700

    created footer draft for desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,79 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-// import Logo from "./logo/Logo"
+import Logo from "./logo/Logo"
 
-// import socMed from "../assets/footer-icons"
+import ig from "../assets/footer-icons/ig.png"
+import linked from "../assets/footer-icons/linked.png"
 
 function Footer(){
     return (
-        <footer>
-            <div>
-                <div> {/* logo */}
-
+        <footer className="bg-exer-charcoal text-white">
+            <div className="pl-24 pr-24 font-poppins">
+                {/* logo */}
+                <div id="exercise" className="pt-8 pb-5 hidden w-[30%] md:flex flex-col gap-8">
+                    <Logo style="w-40"/>
                 </div>
 
-                <div> {/* 3 columns of text */}
-                    
-                    <div> {/* column 1 */}
+                {/* contents */}
+                <div className="pb-6 flex flex-row justify-between">
+                    {/* column 1 */}
+                    <div className="hidden w-[30%] flex-col md:flex gap-4"> 
                         <p className="font-light text-justify">
-                        Creating EXERCISE 2023 to be a career catalyst and a platform to 
-                        inspire and help members apply knowledge and develop relevant skills 
+                        Creating EXERCISE 2024 to be a career catalyst and a platform to
+                        inspire and help members apply knowledge and develop relevant skills
                         in an adaptive and progressive environment.
                         </p>
 
                         <div className="font-bold">
-                            <p>#JourneyOfGrowth</p>
-                            <p>#AdaptiveRelevantProfessional</p>
+                        <p>#ExercisingTheFuture</p>
+                        <p>#AdaptiveRelevantProfessional</p>
                         </div>
-
                     </div>
 
-                    <div> {/* column 2 */}
-                        <p className="font-bold">Our Location</p>
-                        <p>
-                            Faculty of Engineering, University of Indonesia, Jl. Prof. DR. Ir R Roosseno, 
-                            Kukusan, Beji, Depok City, West Java 16425
-                        </p>
+                    {/* column 2 */}
+                    <div className="hidden w-[30%] flex-col md:flex gap-4"> 
                         <div>
-                            <p className="font-bold">Follow Us</p>
-                            {/* SocMed here */}
-                            
+                            <p className="font-bold">Our Location</p>
+                            <p className="font-light text-justify">
+                            Faculty of Engineering, University of Indonesia, 
+                            Jl. Prof. DR. Ir R Roosseno, Kukusan, Beji, Depok City, West Java 16425</p>
                         </div>
+                        
+                        <div className="flex flex-row gap-12">
+                            <div className="flex-col">
+                                <p className="font-bold">Follow Us</p>
+                                <div className="flex flex-row gap-2">
+                                    <a href="https://www.instagram.com/exercise.ftui/"><img src={ig} className="flex-col object-contain w-10 h-10"></img></a>
+                                    <a href="https://www.linkedin.com/company/exercise-ftui/mycompany/"><img src={linked} className="flex-col object-contain w-10 h-10"></img></a>
+                                </div>
 
-                        <div>
-                            <p className="font-bold">Our Contact</p>
-                            <p>Alsha (08176972056)</p>
+                                
+                            </div>
+
+                            <div className="flex-col">
+                                <p className="font-bold">Our Contact</p>
+                                <p>Alsha (08176972056)</p>
+                            </div>
                         </div>
-
                     </div>
 
-                    <div> {/* column 3 */}
+                    {/* column 3 */}
+                    <div className="hidden w-[30%] flex-col md:flex gap-1">
                         <p className="font-bold">Newsletter</p>
-                        <div className="flex flex-col gap-2 mb-2 ">
-                            <textarea
-                                placeholder="name@example.com"
-                                className="resize-none h-10 text-dark-2 text-sm leading-10 pl-2"
-                            />
-                            <Link to="/newsletter" className="btn-dark rounded-md bg-dark-2">
-                                SUBMIT
-                            </Link>
-                        </div>
+                        <input className="rounded-full pl-2 italic h-8 shadow-inner" placeholder="name@example.com">
+                        </input>
                     </div>
-
                 </div>
             </div>
+            
 
-
-
-            <div> {/* bottom */} 
+            {/* copyright */}
+            <div className="bg-exer-gunmetal flex-col md:flex gap-4 font-poppins">
+                <div className="pt-1 pb-1 pl-24 pr-24 font-semibold flex flex-row justify-between">
+                    <p>Copyright © 2024. Exercise FTUI</p>
+                    <p className="">Powered by React JS</p>
+                </div>
             </div>
         </footer>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      "colors":{
+        "exer": {
+          "charcoal" : "#0D2734",
+          "gunmetal" : "#15394A",
+          "darkelectricblue" : "#1C465C",
+          "grayblue" : "#527182",
+          "gray" : "#BFC8CE",
+          "whitegray" : "#D9D9D9",
+          "whiteaccent" : "#F5F5F5",
+          "white" : "FAFAFA"
+        }
+      },
+      fontFamily: {
+        poppins : ["Poppins", "sans-serif"],
+        inter : ["Inter", "sans-serif"]
+      },
+    },
   },
   plugins: [],
 }
